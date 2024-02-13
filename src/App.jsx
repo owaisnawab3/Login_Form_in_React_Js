e3a1ac73a8f37b6a20733951fb1b8bdccaaac7bc
owais3 <khan6awais@gmail.com>
2024-02-13 15:29:06 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginFoam from './Componets/LoginFoam/LoginFoam'
+
+function App() {
+
+  return (
+    <>
+      <LoginFoam />
+    </>
+  )
+}
+
+export default App
